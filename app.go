732199d820600c32cdaabbbda04c12faa3d044bf
Oğuz Han Özmen <oushan16@gmail.com>
2022-03-06 00:46:12 +0300

    seperating routes

--- app.go
+++ app.go
@@ -1,15 +1,13 @@
 package main
 
 import (
+	"fiberexample/routes"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
+	routes.Setup(app)
 
 	app.Listen(":3000")
 }
