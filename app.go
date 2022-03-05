86ab5c1d1589d6307340d3bdaad0e860b0e19322
Oğuz Han Özmen <oushan16@gmail.com>
2022-03-06 00:46:37 +0300

    implementing dotenv

--- app.go
+++ app.go
@@ -3,11 +3,21 @@ package main
 import (
 	"fiberexample/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	bootstrap()
+	
 	app := fiber.New()
 	routes.Setup(app)
 
 	app.Listen(":3000")
+}
+
+func bootstrap() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module fiberexample
 
 go 1.17
 
-require github.com/gofiber/fiber/v2 v2.28.0
+require (
+	github.com/gofiber/fiber/v2 v2.28.0
+	github.com/joho/godotenv v1.4.0
+)
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
